04d97c72d40ee14c7fb2316059472624b78bb257
Timik <adeyemitimmy@gmail.com>
2020-06-05 02:04:26 +0100

    mind your be

---
+++ index.js
@@ -0,0 +1,35 @@
+let canvas = document.getElementById("etchDesign");
+let height =Number(document.getElementById("input_height").value);
+let width =Number(document.getElementById("input_width").value);
+let sizePicker = document.getElementById("sizePicker");
+let color = document.getElementById("colorPicker");
+let myRow,myColum;
+color.addEventListener("mouseover", function(){});
+sizePicker.onsubmit = function(event){
+    event.preventDefault();
+    clearGrid();
+    makeGrid();
+};
+
+function clearGrid(){
+  while (canvas.firstChild){
+       canvas.removeChild(canvas.firstChild);
+  }
+}
+function makeGrid(){
+for(let i=0;i<height;i++){
+  myRow=document.createElement('div');
+  myRow.className="Rows";
+  for (var j = 0; j < width; j++) {
+  myColum=document.createElement('div');
+  myColum.className="columns";
+  myRow.appendChild(myColum);
+  myColum.addEventListener("mouseover", fillSquare);
+  }
+  canvas.appendChild(myRow)
+}
+}
+function fillSquare () {
+  this.setAttribute("style", `background-color: ${color.value}`);
+}
+
